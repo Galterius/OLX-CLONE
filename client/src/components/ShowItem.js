3b11ee275d2db429cd0f3b431e55d23e853ee22e
Pataki Norbert <nonorbi2@gmail.com>
2021-03-18 23:37:36 +0200

    error message cleared

--- client/src/components/ShowItem.js
+++ client/src/components/ShowItem.js
@@ -8,16 +8,16 @@ export const ShowItem = (props) =>{
     return(
         <div>
             
-            <h3 >{listing.title}</h3>
-            <img alt ="" src={listing.image} />
-            <p>{listing.description}</p>
+            <h3 >{props.listing.title}</h3>
+            <img alt ="" src={props.listing.image} />
+            <p>{props.listing.description}</p>
 
-            {(user?.result?.googleId === listing?.creator || user?.result?._id === listing?.creator) && (
+            {(user?.result?.googleId === props.listing?.creator || user?.result?._id === props.listing?.creator) && (
             <div>
                 <button className="btn btn-danger" onClick={props.handleDeleteClick}>Delete listing</button>
                 <Link to={{
-                    pathname: `/listing/edit/${listing._id}`,
-                    state: listing
+                    pathname: `/listing/edit/${props.listing._id}`,
+                    state: props.listing
                 }}>
                     <p className="btn btn-warning">Edit</p>
                 </Link>
@@ -30,13 +30,13 @@ export const ShowItem = (props) =>{
                     <button className="btn btn-success">Add Comment</button>
                 </form>
             </div>
-            {listing?.comments?.map(comment =>
+            {props.listing?.comments?.map(comment =>
                 <div className="d-flex justify-content-center mb-3" key={comment._id}>
                 <div className="card" style={{width: 18 + 'rem'}}>
                     <div className="card-body">
                         <h6 className="card-subtitle mb-2 text-muted">{comment.author.commenterName}</h6>
                         <p className="card-text">{comment.comment}</p>
-                    {(user?.result?.googleId === listing?.creator || user?.result?._id === listing?.creator) && (
+                    {(user?.result?.googleId === props.listing?.creator || user?.result?._id === props.listing?.creator) && (
                         <div> 
                             <button className="card-link btn btn-primary">Edit</button>
                             <button className="card-link btn btn-danger" value={comment._id} onClick={props.handleDeleteComment}>Delet</button>
